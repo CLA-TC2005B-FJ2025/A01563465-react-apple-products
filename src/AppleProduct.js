1d744b6c09584f1f77603c886028465a60feda57
Jesús Alexander Herrera Acevedo <152633538+ExplosiveSmile05@users.noreply.github.com>
2025-04-08 16:25:04 +0000

    Tengo hambre

--- src/AppleProduct.js
+++ src/AppleProduct.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 import iphone from "./svg/iphone.svg"
 import ipad from "./svg/iphone.svg"
@@ -6,15 +7,26 @@ import watch from "./svg/iphone.svg"
 import imac from "./svg/imac.svg"
 import macbook from "./svg/macbook.svg"
 import airpods from "./svg/airpods.svg"
+import heart from "./svg/heart.svg"
+import "./AppleProduct.css"
 
 const mapaSVG = {iphone, ipad, watch, imac, macbook, airpods};
 
 
 export function AppleProduct( props ) {
+  const [clicks, setClicks] = useState(0);
+  const manejarclick = () => {
+    setClicks(clicks + 1);
+  }
+
   return (
-    <div>
-      Producto {props.type}
-      <img alt="producto" src={mapaSVG[props.type]} width={200}/>
+    <div ClassName='apple-product' onClick={manejarclick}>
+      <img ClassName='product'
+      alt="producto" src={mapaSVG[props.type]}
+      width={100}/>
+      <img ClassName='heart'
+       alt="heart" src={heart} 
+       style={{width:10+10*clicks+ 'px'}}/>
     </div>
   )
 }
