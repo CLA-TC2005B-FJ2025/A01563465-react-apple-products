648524a861c93b6d8cf5c783857d1e7458975522
Jesús Alexander Herrera Acevedo <152633538+ExplosiveSmile05@users.noreply.github.com>
2025-04-08 15:45:23 +0000

    Tengo sueño

--- src/AppleProduct.js
+++ src/AppleProduct.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import iphone from "./svg/iphone.svg"
+import ipad from "./svg/iphone.svg"
+import watch from "./svg/iphone.svg"
+import imac from "./svg/imac.svg"
+import macbook from "./svg/macbook.svg"
+import airpods from "./svg/airpods.svg"
+
+const mapaSVG = {iphone, ipad, watch, imac, macbook, airpods};
+
+
 export function AppleProduct( props ) {
   return (
     <div>
       Producto {props.type}
+      <img alt="producto" src={mapaSVG[props.type]} width={200}/>
     </div>
   )
 }
